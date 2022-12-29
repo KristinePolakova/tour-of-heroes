d27f78010a84c446f6c8b10b1191aacad9e90a5f
Polakova <kristine.polakova@accenture.com>
2022-12-29 19:04:04 +0200

    made after angular documentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/components/heroes/heroes.component';
+import { HeroeDetailComponent } from './heroes/components/heroe-detail/heroe-detail.component';
+import { MessagesComponent } from './heroes/components/messages/messages.component';
+import { DashboardComponent } from './heroes/components/dashboard/dashboard.component';
+import { HeroSearchComponent } from './heroes/components/hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/heroes/components/heroe-detail/heroe-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Hero } from '../../types/hero.interface';
+import { HeroService } from '../../services/hero.service';
+
+@Component({
+  selector: 'app-heroe-detail',
+  templateUrl: './heroe-detail.component.html',
+  styleUrls: ['./heroe-detail.component.css'],
+})
+export class HeroeDetailComponent implements OnInit {
+  hero: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero: any) => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
+  }
+}
